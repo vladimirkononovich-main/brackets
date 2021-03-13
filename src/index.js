11f7220828e3d59427d776a23a10ebf5039bc99c
vladimirkononovich-main <vova.kononovich.main@gmail.com>
2021-03-14 02:37:41 +0300

    feat: change

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = bracketsConfig.map( config => config.join('') );
+   for (let i = 0; i < config.length; i++) {
+    if( str.includes(config[i]) ) {
+       str = str.replace(config[i], '');
+       i = -1;
+     }
+   }
+   return !str;
 }
